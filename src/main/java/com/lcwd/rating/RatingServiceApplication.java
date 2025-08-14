a72b7531bbb3d0b27b1a8c609525176a268a8779
Shubham <shubhamkumarhms13@gmail.com>
2025-08-14 23:12:28 +0530

    Added Eureka Server changes

--- src/main/java/com/lcwd/rating/RatingServiceApplication.java
+++ src/main/java/com/lcwd/rating/RatingServiceApplication.java
@@ -2,12 +2,12 @@ package com.lcwd.rating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RatingServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RatingServiceApplication.class, args);
 	}
-
 }
