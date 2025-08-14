223216679e975eccbaa98d2bd3733309ac69d387
Shubham <shubhamkumarhms13@gmail.com>
2025-08-15 00:19:06 +0530

    Changes related to Feign Client and Tested few interfaces

---
+++ src/main/java/com/lcwd/user/service/external/services/RatingService.java
@@ -0,0 +1,21 @@
+package com.lcwd.user.service.external.services;
+
+import com.lcwd.user.service.entities.Rating;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+@Service
+@FeignClient(name = "RATING-SERVICE")
+public interface RatingService {
+
+    @PostMapping("/ratings")
+    Rating createRating(Rating rating);
+
+    @PutMapping("/ratings/{ratingId}")
+    ResponseEntity<Rating> updateRating(@PathVariable("ratingId") String ratingId, Rating rating);
+
+    @DeleteMapping("/ratings/{ratingId}")
+    void deleteRating(@PathVariable("ratingId") String ratingId);
+}
