1b802a892ce6c95d42c6544edfeac1dbd24edef4
Shubham <shubhamkumarhms13@gmail.com>
2025-08-15 18:31:26 +0530

    Externalise config and added API Gateway

---
+++ src/main/java/com/lcwd/hotel/controllers/StaffController.java
@@ -0,0 +1,21 @@
+package com.lcwd.hotel.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> getAllStaff(){
+        List<String> list = Arrays.asList("Ram", "Shyam", "Sita", "Mohan");
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+}
